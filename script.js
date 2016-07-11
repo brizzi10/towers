e4c49544893004e513e0734789044c86bfa69f93
Ben Brizzi <brizzitime@gmail.com>
2016-07-11 15:41:19 -0400

    got the preview kinda working

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
   // $("body").css("background", "red");
   var isLit = false;
   $(".one").on("click", function(evt){
+    var selectedColumn = ".first";
+    var selectedDisk = "one";
     evt.preventDefault();
-    console.log(this);
     isLit = highlight(".one", isLit);
-
+    discPreview(selectedColumn, selectedDisk);
   })
 
 
@@ -19,4 +20,34 @@ function highlight(discNumber, isLit){
     $(discNumber).css("border-color", "red");
     return true;
   }
+}
+
+function discPreview(selectedColumn, selectedDisk){
+  if (selectedColumn==".first"){
+    optionOne = ".second";
+    optionTwo = ".third";
+  }
+  else if(selectedColumn==".second"){
+    optionOne = ".first";
+    optionTwo = ".third";
+  }
+  else {
+    optionOne = ".first";
+    optionTwo = ".second";
+  }
+  $(optionOne).on("mouseenter", function(){
+    var newDisc =  $("<div class='disc one preview'></div>");
+    $(".tower-2").append(newDisc);
+    $(optionOne).on("mouseleave", function(){
+      $(".disc.one.preview").remove();
+    })
+  })
+
+  $(optionTwo).on("mouseenter", function(){
+    var newDisc = $("<div class='disc one preview'></div>");
+    $(".tower-3").append(newDisc);
+    $(optionTwo).on("mouseleave", function(){
+      $(".disc.one.preview").remove();
+    })
+  })
 }
