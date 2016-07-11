621d97142de188e21eff01fda708edfe9207aa77
Ben Brizzi <brizzitime@gmail.com>
2016-07-11 13:41:04 -0400

    added isLit function to highlight click

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
-  $("body").css("background", "red");
-  console.log("javascript is working!!!");
+  // $("body").css("background", "red");
+  var isLit = false;
+  $(".one").on("click", function(evt){
+    evt.preventDefault();
+    isLit = highlight(".one", isLit);
+  })
+
+
 })
+function highlight(discNumber, isLit){
+  if (isLit){
+    $(discNumber).css("border-color", "rgb(35, 15, 93)");
+    return false;
+  }
+  else{
+    $(discNumber).css("border-color", "red");
+    return true;
+  }
+}
