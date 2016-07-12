ccc723df6575c522379bde471c72e5b2739b4df4
Ben Brizzi <brizzitime@gmail.com>
2016-07-11 21:43:20 -0400

    haven't committed in a while

--- script.js
+++ script.js
@@ -1,28 +1,67 @@
 $(document).ready(function(){
   var isLit = false;
-  $(".one").on("click", function(evt){
+  startingDisks=4;
+  $(".up").on("click", function(){
+    startingDisks = difficulty("up", startingDisks);
+  })
+  $(".down").on("click", function(){
+    startingDisks =difficulty("down", startingDisks);
+  })
+  $(".reset").on("click", function(){
+    startingDisks = 4;
+    reset();
+  })
+  $(".one").on("click", function(){
     var selectedColumn = ".first";
     var selectedDisk = "one";
-    evt.preventDefault();
     isLit = highlight(selectedDisk, isLit);
-    discPreview(selectedColumn, selectedDisk);
+    diskPreview(selectedColumn, selectedDisk);
+  })
+
+  $(".two").on("click", function(){
+    var selectedColumn = ".first";
+    var selectedDisk = "two";
+    isLit = highlight(selectedDisk, isLit);
+    diskPreview(selectedColumn, selectedDisk);
+  })
+
+  $(".three").on("click", function(){
+    var selectedColumn = ".first";
+    var selectedDisk = "three";
+    isLit = highlight(selectedDisk, isLit);
+    diskPreview(selectedColumn, selectedDisk);
+  })
+
+  $(".four").on("click", function(){
+    var selectedColumn = ".first";
+    var selectedDisk = "four";
+    isLit = highlight(selectedDisk, isLit);
+    diskPreview(selectedColumn, selectedDisk);
+  })
+
+  $(".five").on("click", function(){
+    var selectedColumn = ".first";
+    var selectedDisk = "five";
+    isLit = highlight(selectedDisk, isLit);
+    diskPreview(selectedColumn, selectedDisk);
   })
 
 
 })
-function highlight(discNumber, isLit){
-  discNumber = "." + discNumber;
+function highlight(selectedDisk, isLit){
+  diskNumber = "." + selectedDisk;
   if (isLit){
-    $(discNumber).css("border-color", "rgb(35, 15, 93)");
+    $(diskNumber).css("border-color", "rgb(35, 15, 93)");
+
     return false;
   }
   else{
-    $(discNumber).css("border-color", "red");
+    $(diskNumber).css("border-color", "red");
     return true;
   }
 }
 
-function discPreview(selectedColumn, selectedDisk){
+function diskPreview(selectedColumn, selectedDisk){
   if (selectedColumn==".first"){
     optionOne = ".second";
     optionTwo = ".third";
@@ -36,17 +75,51 @@ function discPreview(selectedColumn, selectedDisk){
     optionTwo = ".second";
   }
   $(optionOne).on("mouseenter", function(){
-    var newDisc =  $("<div class='disc " + selectedDisk + " preview'></div>");
-    $(optionOne).append(newDisc);
-    $(optionOne).on("mouseleave", function(){
-      $(".disc.one.preview").remove();
-    })
+    var newdisk =  $("<div class='disk " + selectedDisk + " preview'></div>");
+    $(optionOne).append(newdisk);
+  })
+  $(optionOne).on("mouseleave", function(){
+    $(".disk." + selectedDisk + ".preview").remove();
   })
   $(optionTwo).on("mouseenter", function(){
-    var newDisc =  $("<div class='disc " + selectedDisk + " preview'></div>");
-    $(optionTwo).append(newDisc);
-    $(optionTwo).on("mouseleave", function(){
-      $(".disc.one.preview").remove();
-    })
+    var newdisk =  $("<div class='disk " + selectedDisk + " preview'></div>");
+    $(optionTwo).append(newdisk);
   })
+  $(optionTwo).on("mouseleave", function(){
+    $(".disk." + selectedDisk + ".preview").remove();
+  })
+}
+function diskMove(selectedColumn, selectedDisk){
+}
+function difficulty(change, startingDisks){
+  if(change=="up"){
+    if(startingDisks<4){
+      startingDisks++;
+      $(".disk").eq(startingDisks).toggle();
+      return startingDisks;
+    }
+    else{
+      console.log("it only goes up to 5 disks, don't be a nerd");
+      return startingDisks;
+    }
+  }
+  else if(change=="down"){
+    if(startingDisks>0){
+      $(".disk").eq(startingDisks).toggle();
+      startingDisks--;
+      return startingDisks;
+    }
+    else{
+      console.log("Yes, I thought of this case. Nice try.");
+      return startingDisks;
+    }
+  }
+}
+function reset(startingDisks){
+  $(".disk.one").show();
+  $(".disk.two").show();
+  $(".disk.three").show();
+  $(".disk.four").show();
+  $(".disk.five").show();
+  return;
 }
