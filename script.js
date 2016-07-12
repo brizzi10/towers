5cf37affb6c2c94dc07382aaa4563432f59b9bed
Ben Brizzi <brizzitime@gmail.com>
2016-07-12 19:39:38 -0400

    added lots of comments

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+  //starting disks at 4 since elements go 0-4
   var startingDisks=4;
+  //each tower is its own object to constantly update info as game is played
   var towerOne={location:".first", topDisk:".one", totalDisks:startingDisks, highlighted:false};
   var towerTwo={location:".second ", topDisk:"", totalDisks: 0, highlighted:false};
   var towerThree={location:".third", topdisk:"", totalDisks: 0, highlighted: false};
-
+//one click event for each tower, selectedTower set on click
   $(".first").on("click", function(){
     var selectedTower = towerOne;
     selectedTower.highlighted = highlight(selectedTower);
@@ -18,7 +20,7 @@ $(document).ready(function(){
     var selectedTower = towerThree;
     isLit = highlight(selectedTower);
   })
-
+//one click event for each button on interface
   $(".up").on("click", function(){
     startingDisks = difficulty("up", startingDisks);
   })
@@ -31,7 +33,7 @@ $(document).ready(function(){
     reset();
   })
 })
-
+//function to highlight the top disk of the selectedTower, returns boolean to update tower object
 function highlight(selectedTower){
   var location = selectedTower.location + " " + selectedTower.topDisk;
   if (selectedTower.highlighted){
@@ -43,8 +45,9 @@ function highlight(selectedTower){
     return true;
   }
 }
-
+//function to create the disk preview image to show upcoming moves
 function diskPreview(selectedTower, towerOne, towerTwo, towerThree){
+  //when a tower is selected, only two moves are possible
   if (selectedTower==towerOne){
     optionOne = towerTwo.location;
     optionTwo = towerThree.location;
@@ -57,15 +60,17 @@ function diskPreview(selectedTower, towerOne, towerTwo, towerThree){
     optionOne = towerOne.location;
     optionTwo = towerTwo.location;
   }
-
+  //combining strings for jquery selector
   var nextLocationOne = optionOne + " " + selectedTower.topDisk;
   var nextLocationTwo = optionTwo + " " + selectedTower.topDisk;
+  //adds the preview class to the relevant tower
   $(nextLocationOne).addClass("preview");
   $(nextLocationTwo).addClass("preview");
-
+  //the preview disk div only toggles when the selectedTower is highlighted
   if(selectedTower.highlighted){
     $(optionOne).on("mouseenter", function(){
       $(nextLocationOne).toggle();
+      //event listener inside mouseenter, since this is when a click to move disk would occur
       $(this).on("click", function(){
         diskMove(selectedTower.topDisk, nextLocationOne);
       })
@@ -73,6 +78,7 @@ function diskPreview(selectedTower, towerOne, towerTwo, towerThree){
     $(optionOne).on("mouseleave", function(){
       $(nextLocationOne).toggle();
     })
+    //move option two is set up identical to option one, since either could be chosen
     $(optionTwo).on("mouseenter", function(){
       $(nextLocationTwo).toggle();
       $(this).on("click", function(){
@@ -83,7 +89,9 @@ function diskPreview(selectedTower, towerOne, towerTwo, towerThree){
       $(nextLocationTwo).toggle();
     })
   }
+  //if the selectedTower is not highlighted
   else {
+    //user must have decided agaisnt there choice, so turn off the preview event listeners
     previewOff(nextLocationOne, nextLocationTwo);
   }
 }
@@ -91,6 +99,7 @@ function diskMove(selectedTower, destination){
   $(selectedTower.topDisk).toggle();
   $(destination).removeClass("preview");
 }
+//function to turn off preview event listeners and remove the preview class from the optionOne and optionTwo divs
 function previewOff(nextLocationOne, nextLocationTwo){
   $(nextLocationOne).removeClass("preview");
   $(nextLocationTwo).removeClass("preview");
@@ -100,6 +109,7 @@ function previewOff(nextLocationOne, nextLocationTwo){
   $(optionTwo).off("mouseenter");
   $(optionTwo).off("mouseleave");
 }
+//function to change the number of disks available
 function difficulty(change, startingDisks){
   if(change=="up"){
     if(startingDisks<4){
@@ -124,6 +134,7 @@ function difficulty(change, startingDisks){
     }
   }
 }
+//resets the game board to its original state(all plates on leftmost tower)
 function reset(startingDisks){
   $(".first .disk.one").show();
   $(".first .disk.two").show();
