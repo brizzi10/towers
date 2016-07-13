0253ac2d08737adb968636f1e1c91175c80e6adf
Ben Brizzi <brizzitime@gmail.com>
2016-07-13 11:41:23 -0400

    new structure for event listeners

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-  //starting disks at 4 since elements go 0-4
   var startingDisks=4;
-  //each tower is its own object to constantly update info as game is played
+
+  var gameState="initial";
+
   var towerOne={
     location:".first",
     topDisk:".one",
@@ -11,220 +12,219 @@ $(document).ready(function(){
     fifthDisk:".five",
     totalDisks:startingDisks,
     highlighted:false};
-  var towerTwo={
-    location:".second ",
-    topDisk:"",
-    secondDisk:"",
-    thirdDisk:"",
-    fourthDisk:"",
-    fifthDisk:"",
-    totalDisks: 0,
-    highlighted:false};
-  var towerThree={
-    location:".third",
-    topdisk:"",
-    secondDisk:"",
-    thirdDisk:"",
-    fourthDisk:"",
-    fifthDisk:"",
-    totalDisks: 0,
-    highlighted: false};
-//one click event for each tower, selectedTower set on click
-  $(".first").on("click", function(){
-    var selectedTower = towerOne;
-    //once a tower is selected, the other on click listeners need to turn off as well
-    turnOff();
-    selectedTower.highlighted = highlight(selectedTower);
-    diskPreview(selectedTower, towerOne, towerTwo, towerThree);
-  })
+    var towerTwo={
+      location:".second ",
+      topDisk:"",
+      secondDisk:"",
+      thirdDisk:"",
+      fourthDisk:"",
+      fifthDisk:"",
+      totalDisks: 0,
+      highlighted:false};
+      var towerThree={
+        location:".third",
+        topdisk:"",
+        secondDisk:"",
+        thirdDisk:"",
+        fourthDisk:"",
+        fifthDisk:"",
+        totalDisks: 0,
+        highlighted: false};
 
-  $(".second").on("click", function(){
-    var selectedTower = towerTwo;
-    turnOff();
-    selectedTower.highlighted = highlight(selectedTower);
-    diskPreview(selectedTower, towerOne, towerTwo, towerThree);
-  })
+        var selectedTower;
+        var destinationTower;
 
-  $(".third").on("click", function(){
-    var selectedTower = towerThree;
-    turnOff();
-    selectedTower.highlighted = highlight(selectedTower);
-    diskPreview(selectedTower, towerOne, towerTwo, towerThree);
-  })
-//one click event for each button on interface
-  $(".up").on("click", function(){
-    startingDisks = difficulty("up", startingDisks);
-  })
-  $(".down").on("click", function(){
-    startingDisks =difficulty("down", startingDisks);
-  })
-  $(".reset").on("click", function(){
-    startingDisks = 4;
-    isLit = false;
-    reset();
-  })
-})
-//function to highlight the top disk of the selectedTower, returns boolean to update tower object
-function highlight(selectedTower){
-  var location = selectedTower.location + " " + selectedTower.topDisk;
-  if (selectedTower.highlighted){
-    $(location).removeClass("highlight");
-    return false;
-  }
-  else{
-    $(location).addClass("highlight");
-    return true;
-  }
-}
-//function to create the disk preview image to show upcoming moves
-function diskPreview(selectedTower, towerOne, towerTwo, towerThree){
-  //when a tower is selected, only two moves are possible
-  if (selectedTower==towerOne){
-    optionOne = towerTwo.location;
-    optionTwo = towerThree.location;
-  }
-  else if(selectedTower==towerTwo){
-    optionOne = towerOne.location;
-    optionTwo = towerThree.location;
-  }
-  else {
-    optionOne = towerOne.location;
-    optionTwo = towerTwo.location;
-  }
-  //combining strings for jquery selector
-  var nextLocationOne = optionOne + " " + selectedTower.topDisk;
-  var nextLocationTwo = optionTwo + " " + selectedTower.topDisk;
+        //one click event for each tower, selectedTower set on click
+        $(".first").on("click", function(){
+          if(gameState=="initial"){
+            selectedTower = towerOne;
+            selectedTower.highlighted = highlight(selectedTower);
+            gameState="preview";
+            diskPreview(selectedTower, towerOne, towerTwo, towerThree);
+          }
+        })
 
-  //the preview disk div only toggles when the selectedTower is highlighted
-  if(selectedTower.highlighted){
-    //adds the preview class to the relevant tower
-    $(nextLocationOne).addClass("preview");
-    $(nextLocationTwo).addClass("preview");
+        $(".second").on("click", function(){
+          if(gameState=="initial"){
+            selectedTower = towerTwo;
+            selectedTower.highlighted = highlight(selectedTower);
+            gameState=="preview";
+            diskPreview(selectedTower, towerOne, towerTwo, towerThree);
+          }
+        })
 
-    $(optionOne).on("mouseenter", function(){
-      $(nextLocationOne).toggle();
-      //event listener inside mouseenter, since this is when a click to move disk would occur
-      $(this).on("click", function(){
-        previewOff(nextLocationOne, nextLocationTwo);
-        diskMove(selectedTower, nextLocationOne);
-        $(this).off("click");
-        $(optionTwo).off("click");
+        $(".third").on("click", function(){
+          if (gameState=="initial"){
+            selectedTower = towerThree;
+            selectedTower.highlighted = highlight(selectedTower);
+            gameState=="preview"
+            diskPreview(selectedTower, towerOne, towerTwo, towerThree);
+          }
+        })
+
+        $(".first").on("mouseenter", function(){
+          if(gameState=="preview" && !(selectedTower==towerOne)){
+            console.log("1 is turned on");
+            $(towerOne.topdisk).toggle();
+          }
+        })
+
+        $(".first").on("mouseleave", function(){
+          if(gameState=="preview" && !(selectedTower==towerOne)){
+            console.log("1 is turned on");
+            $(towerOne.topdisk).toggle();
+          }
+        })
+
+        $(".second").on("mouseenter", function(){
+          if(gameState=="preview" && !(selectedTower==towerTwo)){
+            $(towerTwo.topdisk).toggle();
+            console.log("2 is turned on");
+          }
+        })
+
+        $(".second").on("mouseleave", function(){
+          if (gameState=="preview" && !(selectedTower==towerTwo)){
+            console.log("2 is turned on");
+            $(towerTwo.topdisk).toggle();
+          }
+        })
+
+        $(".third").on("mouseenter", function(){
+          if(gameState=="preview" && !(selectedTower==towerThree)){
+            console.log("3 is turned on");
+            $(towerThree.topdisk).toggle();
+          }
+        })
+
+        $(".third").on("mouseleave", function(){
+          if (gameState=="preview" && !(selectedTower==towerThree)){
+            console.log("3 is turned on");
+            $(towerThree.topdisk).toggle();
+          }
+        })
+
+        //one click event for each button on interface
+        $(".up").on("click", function(){
+          startingDisks = difficulty("up", startingDisks);
+        })
+        $(".down").on("click", function(){
+          startingDisks =difficulty("down", startingDisks);
+        })
+        $(".reset").on("click", function(){
+          startingDisks = 4;
+          isLit = false;
+          reset();
+        })
       })
-    })
-    $(optionOne).on("mouseleave", function(){
-      $(nextLocationOne).toggle();
-    })
-    //move option two is set up identical to option one, since either could be chosen
-    $(optionTwo).on("mouseenter", function(){
-      $(nextLocationTwo).toggle();
-      $(this).on("click", function(){
-        previewOff(nextLocationOne, nextLocationTwo);
-        diskMove(selectedTower, nextLocationTwo);
-        $(this).off("click");
-        $(optionOne).off("click");
-      })
-    })
-    $(optionTwo).on("mouseleave", function(){
-      $(nextLocationTwo).toggle();
-    })
-  }
-  //if the selectedTower is not highlighted
-  else {
-    //user must have decided agaisnt their choice, so turn off the preview event listeners
-    previewOff(nextLocationOne, nextLocationTwo);
-  }
-}
-function diskMove(selectedTower, destination, towerOne, towerTwo, towerThree){
-  var originalDisk = selectedTower.location + " " + selectedTower.topDisk;
-  //toggles the original disk off and removes highlight
-  $(originalDisk).toggle();
-  $(originalDisk).removeClass("highlight");
-  //reveals the moved disk by removing the preview css
-  // $(destination).removeClass("preview");
-  //tower objects updated: original loses disk and highlight
-  updateTowers(selectedTower, destination);
-}
-//function to update tower objects after move
-function updateTowers(selectedTower, destinationTower){
 
-  selectedTower.highlight = false;
-  selectedTower.totalDisks--;
-  destinationTower.totalDisks++;
+      //function to highlight the top disk of the selectedTower, returns boolean to update tower object
+      function highlight(selectedTower){
+        var location = selectedTower.location + " " + selectedTower.topDisk;
+        if (selectedTower.highlighted){
+          $(location).removeClass("highlight");
+          return false;
+        }
+        else{
+          $(location).addClass("highlight");
+          return true;
+        }
+      }
+      //function to create the disk preview image to show upcoming moves
+      function diskPreview(selectedTower, towerOne, towerTwo, towerThree){
+        //when a tower is selected, only two moves are possible
+        if (selectedTower==towerOne){
+          var optionOne = towerTwo.location;
+          var optionTwo = towerThree.location;
+        }
+        else if(selectedTower==towerTwo){
+          var optionOne = towerOne.location;
+          var optionTwo = towerThree.location;
+        }
+        else {
+          var optionOne = towerOne.location;
+          var optionTwo = towerTwo.location;
+        }
+        //combining strings for jquery selector
+        previewDiskOne = optionOne + " " + selectedTower.topDisk;
+        previewDiskTwo = optionTwo + " " + selectedTower.topDisk;
 
-  destinationTower.fifthDisk = destinationTower.fourthDisk;
-  destinationTower.fourthDisk = destinationTower.thirdDisk;
-  destinationTower.thirdDisk = destinationTower.secondDisk;
-  destinationTower.secondDisk = destinationTower.topDisk;
-  destinationTower.topDisk = selectedTower.topDisk;
+        //adds the preview class to the relevant tower
+        $(previewDiskOne).addClass("preview");
+        $(previewDiskTwo).addClass("preview");
+      }
 
-  selectedTower.topDisk = selectedTower.secondDisk;
-  selectedTower.secondDisk = selectedTower.thirdDisk;
-  selectedTower.thirdDisk = selectedTower.fourthDisk;
-  selectedTower.fourthDisk = selectedTower.fifthDisk;
-  selectedTower.fifthDisk = "";
+      function diskMove(selectedTower, destination, towerOne, towerTwo, towerThree){
+        var originalDisk = selectedTower.location + " " + selectedTower.topDisk;
+        //toggles the original disk off and removes highlight
+        $(originalDisk).toggle();
+        $(originalDisk).removeClass("highlight");
+        $(destination).removeClass("preview");
+      }
+      //function to update tower objects after move
+      function updateTowers(selectedTower, destinationTower){
 
-}
-//function to turn off preview event listeners and remove the preview class from the optionOne and optionTwo divs
-function previewOff(nextLocationOne, nextLocationTwo){
-  $(nextLocationOne).removeClass("preview");
-  $(nextLocationTwo).removeClass("preview");
+        selectedTower.highlight = false;
+        selectedTower.totalDisks--;
+        destinationTower.totalDisks++;
 
-  $(optionOne).off("mouseenter");
-  $(optionOne).off("mouseleave");
-  $(optionTwo).off("mouseenter");
-  $(optionTwo).off("mouseleave");
+        destinationTower.fifthDisk = destinationTower.fourthDisk;
+        destinationTower.fourthDisk = destinationTower.thirdDisk;
+        destinationTower.thirdDisk = destinationTower.secondDisk;
+        destinationTower.secondDisk = destinationTower.topDisk;
+        destinationTower.topDisk = selectedTower.topDisk;
 
-}
-//function to change the number of disks available
-function difficulty(change, startingDisks){
-  if(change=="up"){
-    if(startingDisks<4){
-      startingDisks++;
-      $(".disk").eq(startingDisks).toggle();
-      return startingDisks;
-    }
-    else{
-      console.log("it only goes up to 5 disks, don't be a nerd");
-      return startingDisks;
-    }
-  }
-  else if(change=="down"){
-    if(startingDisks>0){
-      $(".disk").eq(startingDisks).toggle();
-      startingDisks--;
-      return startingDisks;
-    }
-    else{
-      console.log("Yes, I thought of this case. Nice try.");
-      return startingDisks;
-    }
-  }
-}
-//function to turn off the other 2 initial event listeners
-function turnOff(){
-    $(".first").off("click");
-    $(".second").off("click");
-    $(".third").off("click");
-}
+        selectedTower.topDisk = selectedTower.secondDisk;
+        selectedTower.secondDisk = selectedTower.thirdDisk;
+        selectedTower.thirdDisk = selectedTower.fourthDisk;
+        selectedTower.fourthDisk = selectedTower.fifthDisk;
+        selectedTower.fifthDisk = "";
 
-//resets the game board to its original state(all plates on leftmost tower)
-function reset(startingDisks){
-  $(".first .disk.one").show();
-  $(".first .disk.two").show();
-  $(".first .disk.three").show();
-  $(".first .disk.four").show();
-  $(".first .disk.five").show();
+      }
+      //function to change the number of disks available
+      function difficulty(change, startingDisks){
+        if(change=="up"){
+          if(startingDisks<4){
+            startingDisks++;
+            $(".disk").eq(startingDisks).toggle();
+            return startingDisks;
+          }
+          else{
+            console.log("it only goes up to 5 disks, don't be a nerd");
+            return startingDisks;
+          }
+        }
+        else if(change=="down"){
+          if(startingDisks>0){
+            $(".disk").eq(startingDisks).toggle();
+            startingDisks--;
+            return startingDisks;
+          }
+          else{
+            console.log("Yes, I thought of this case. Nice try.");
+            return startingDisks;
+          }
+        }
+      }
 
-  $(".second .disk.one").hide();
-  $(".second .disk.two").hide();
-  $(".second .disk.three").hide();
-  $(".second .disk.four").hide();
-  $(".second .disk.five").hide();
+      //resets the game board to its original state(all plates on leftmost tower)
+      function reset(startingDisks){
+        $(".first .disk.one").show();
+        $(".first .disk.two").show();
+        $(".first .disk.three").show();
+        $(".first .disk.four").show();
+        $(".first .disk.five").show();
 
-  $(".second .disk.one").hide();
-  $(".second .disk.two").hide();
-  $(".second .disk.three").hide();
-  $(".second .disk.four").hide();
-  $(".second .disk.five").hide();
-  return;
-}
+        $(".second .disk.one").hide();
+        $(".second .disk.two").hide();
+        $(".second .disk.three").hide();
+        $(".second .disk.four").hide();
+        $(".second .disk.five").hide();
+
+        $(".second .disk.one").hide();
+        $(".second .disk.two").hide();
+        $(".second .disk.three").hide();
+        $(".second .disk.four").hide();
+        $(".second .disk.five").hide();
+        return;
+      }
